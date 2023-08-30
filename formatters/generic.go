7a592a8e426a72b20811c63a40c934667fd4404c
Jose R. Gonzalez <komish@flutes.dev>
2023-08-30 12:38:49 -0500

    replace poc text formatting with poc json formatting to allow submission testing

---
+++ formatters/generic.go
@@ -0,0 +1,49 @@
+package formatters
+
+import (
+	"context"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+
+	"github.com/opdev/knex/types"
+)
+
+var (
+	jsonMarshalIndent = json.MarshalIndent
+	xmlMarshalIndent  = xml.MarshalIndent
+)
+
+// genericJSONFormatter is a FormatterFunc that formats results as JSON
+func genericJSONFormatter(ctx context.Context, r types.Results) ([]byte, error) {
+	response := getResponse(r)
+
+	responseJSON, err := jsonMarshalIndent(response, "", "    ")
+	if err != nil {
+		e := fmt.Errorf("error formatting results with formatter %s: %w",
+			"json",
+			err,
+		)
+
+		return nil, e
+	}
+
+	return responseJSON, nil
+}
+
+// genericXMLFormatter is a FormatterFunc that formats results as XML
+func genericXMLFormatter(ctx context.Context, r types.Results) ([]byte, error) {
+	response := getResponse(r)
+
+	responseXML, err := xmlMarshalIndent(response, "", "    ")
+	if err != nil {
+		e := fmt.Errorf("error formatting results with formatter %s: %w",
+			"xml",
+			err,
+		)
+
+		return nil, e
+	}
+
+	return responseXML, nil
+}
